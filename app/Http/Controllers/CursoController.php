8fffcf041fddfa58895419ced5948c1702db8701
Noelia Martínez <noeowo384@gmail.com>
2021-06-08 13:23:52 -0400

    comentando evaluacion usuario

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -362,11 +362,16 @@ class CursoController extends Controller
             $usuarioCurso->estado_usuario_curso = 'adquirido';
             $usuarioCurso->save();
 
-			$result = new UsuarioEvaluacion;
-				$result->id_curso = $usuarioCurso->id_curso;
-				$result->id_usuario = $usuarioCurso->id_usuario;
-				$result->progreso_evaluacion = 0;
-				$result->save();
+			// $examen = UsuarioEvaluacion::where('id_usuario', $usuarioCurso->id_usuario)
+			// 					->where('id_curso', $usuarioCurso->id_curso)
+			// 					->get();
+			// if(count($examen) == 0){
+			// 	$result = new UsuarioEvaluacion;
+			// 	$result->id_curso = $usuarioCurso->id_curso;
+			// 	$result->id_usuario = $usuarioCurso->id_usuario;
+			// 	$result->progreso_evaluacion = 0;
+			// 	$result->save();
+			// }
 
 			$solicitudesAnteriores =  UsuarioCurso::where('id_usuario', $usuarioCurso->id_usuario)
                 ->where('id_curso', $usuarioCurso->id_curso)
